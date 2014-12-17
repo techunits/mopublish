33c129c2926e3723e05cd42c575025205db8845e
Sougata Pal <skall.paul@techunits.com>
2014-12-18 00:33:17 +0530

    update final changes in admin panel

--- themes/mp-manager/js/custom/resource.js
+++ themes/mp-manager/js/custom/resource.js
@@ -20,7 +20,10 @@ mpmgrApp.controller('SiteSettings', function() {
 	$('form[name="siteSettings"]').validate();
 });
 
-mpmgrApp.controller('UpdateContent', function() {
+/**
+ * action controllers for UpdateContent view
+ */
+mpmgrApp.controller('UpdateContent', function($scope, $http) {
 	//	initialize tinymce for the editor.
 	tinymce.init({
 		selector:'textarea.maineditor',
@@ -30,6 +33,8 @@ mpmgrApp.controller('UpdateContent', function() {
 	
 	//	add form validations
 	$('form[name="updateContent"]').validate();
+	
+	//	datepicker
 	
 	//	data auto replication
 	$('form[name="updateContent"]	input[name="title"]').focusout(function() {
@@ -54,8 +59,72 @@ mpmgrApp.controller('UpdateContent', function() {
 	});
 	
 	
-	//	gmap handlers
+	function setGeoCodingInfo(geoaddress) {
+		$scope.lat = geoaddress.geometry.location.lat();
+		$scope.lng = geoaddress.geometry.location.lng();
+		
+		var latlngObj = new google.maps.LatLng($scope.lat, $scope.lng);
+		map.panTo(latlngObj);
+		marker.setPosition(latlngObj);
+		$('input[name="lat"]').focus();
+	}
 	
+	/**
+	 * initialize map and set default marker
+	 */
+    $scope.lat = $('input[name="lat"]').val();
+    $scope.lng = $('input[name="lng"]').val();
+    var geocoder = new google.maps.Geocoder();
+    var latlngObj = new google.maps.LatLng($scope.lat, $scope.lng);
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		center: latlngObj,
+		zoom: 12
+	});
+	var marker = new google.maps.Marker({
+		map: map,
+		position: latlngObj,
+		title: "Current Location",
+		animation: google.maps.Animation.DROP,
+		draggable: true
+	});
+	
+	google.maps.event.addListener(map, 'click', function(selectedMarker) {
+		$scope.searchterm = null;
+		$scope.lat = selectedMarker.latLng.lat();
+		$scope.lng = selectedMarker.latLng.lng();
+		
+		var latlngObj = new google.maps.LatLng($scope.lat, $scope.lng);
+		map.panTo(latlngObj);
+		marker.setPosition(latlngObj);
+		
+		$('input[name="lat"]').focus();
+	});
+	
+	google.maps.event.addListener(marker, 'dragend', function(selectedMarker) {
+		$scope.searchterm = null;
+		$scope.lat = selectedMarker.latLng.lat();
+		$scope.lng = selectedMarker.latLng.lng();
+		
+		$('input[name="lat"]').focus();
+	});
+	
+	$('input[readonly="readonly"]').click(function() {
+		alert('Please drag marker on map to choose the exact location.');
+	});
+	
+	/**
+	 * term search for map frame
+	 */
+	$scope.searchLocation = function($event) {
+		geocoder.geocode({'address': $scope.searchterm}, function(results, status) {
+			if(google.maps.GeocoderStatus.OK == status && results[0]) {
+				setGeoCodingInfo(results[0]);
+			}
+			else {
+				alert('Sorry!!! Location not found, please try other locations again.');
+			}
+		});
+	};
 });
 
 mpmgrApp.controller('ContentTypeSettings', function() {
